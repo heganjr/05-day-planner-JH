68e73aff501f05a4d7eff155289a24e6e020a852
heganjr <jacobhegan@hotmail.com>
2021-04-09 00:10:45 +0800

    added time in heading add localStorage when adding items in time slots added func colourCodingTimeSlots added timeInterval to refresh every 60 secs to update colours script ready for submission

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-$(document).ready(
-    // fucntions run once the document is ready
+date = moment().format("MMMM Do YYYY");
+console.log(date);
 
-    $('.saveBtn').on('click',
-        function() {
-            console.log('hello')
+$("#currentDay").text(date);
 
-            let value = $(this).siblings('.description').val()
-            console.log(value)
+$(document).ready(function () {
+  // fucntions run once the document is ready
 
-            let time = $(this).siblings('.hour').text()
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    console.log(event);
+    // whenever there is
+    // stop refresh from not working
+    console.log("hello");
 
-            localStorage.setItem(time, value)
-        }
-    
-    
-    ),
+    let textAreaValue = $(this).siblings(".description").val();
+    console.log(textAreaValue);
+    // This referes to the element that registered the event
 
-    $(".hour-9 .description").val(localStorage.getItem("9AM"))
-    
-    )
+    let time = $(this).parent().attr("id");
+
+    console.log(time);
+
+    localStorage.setItem(time, textAreaValue);
+  });
+
+  for (let i = 9; i < 18; i++) {
+    var id = "#hour-" + i;
+
+    //   var id = `#hour-${i}`
+    var textArea = $(id + " .description");
+    textArea.val(localStorage.getItem(id));
+  }
+  //   local storage for each hour section
+
+  console.log(timeOfUser);
+
+  colourCodingTimeSlots();
+  // get this function to run perodically
+  setInterval(colourCodingTimeSlots, 60000);
+});
+
+var timeOfUser = moment().hour();
+
+function colourCodingTimeSlots() {
+  $(".time-block").each(function () {
+    let timeBlock = $(this).attr("id").split("hour-")[1];
+    if (timeBlock < timeOfUser) {
+      $(this).addClass("past");
+    } else if (timeBlock == timeOfUser) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+    console.log(timeBlock);
+    // this and each is keeping track of where we are at in the function
+  });
+}
